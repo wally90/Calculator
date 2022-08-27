524c537e57de48362b6e3eb001ca9c7758c51615
vitaly <maskal412@mail.ru>
2022-08-27 18:46:00 +0300

    step 1

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<View>(R.id.button1).setOnClickListener {
+            Toast.makeText(applicationContext, "bat1", Toast.LENGTH_SHORT).show()
+        }
     }
+
 }
